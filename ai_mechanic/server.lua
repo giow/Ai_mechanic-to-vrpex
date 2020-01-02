96b0966039b661d0594e889a9cd2a6eeb544cdfa
Noah <59400817+Son-Noah@users.noreply.github.com>
2020-01-02 18:39:46 -0300

    Update server.lua

--- ai_mechanic/server.lua
+++ ai_mechanic/server.lua
@@ -10,7 +10,7 @@ AddEventHandler('esx_gym:vahicle', function()
 	local user_id = vRP.getUserId(source)
 	
 	if vRP.tryFullPayment(user_id, 3000) then
-		vRPclient.notify(source,"~r~Você pagou pelo serviço do mecanico.")
+	  vRPclient.notify(source,"~r~Você pagou pelo serviço do mecanico.")
 	else
 		vRPclient.notify(source,"~r~Dinheiro insuficiente.")
 	end 
